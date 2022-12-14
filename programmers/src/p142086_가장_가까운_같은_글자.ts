7bc348d4f03a75e2825d8116052ee10c9af5cdde
Aierse <dbsgur2008@gmail.com>
2022-12-15 02:41:14 +0900

    refactor - export 적용

--- programmers/src/p142086_가장_가까운_같은_글자.ts
+++ programmers/src/p142086_가장_가까운_같은_글자.ts
@@ -1,19 +1,17 @@
-namespace p142086 {
-    function solution(s: string): Array<number> {
-        let solve: number[] = Array<number>(s.length).fill(-1);
-    
-        [...s].forEach((char, index) => {
-            for (let j = index - 1; j >= 0; j--) {
-                if (char === s[j]) {
-                    solve[index] = index - j
-                    break
-                }
-            }
-        })
-    
-        return solve
-    }
+export function solution(s: string): Array<number> {
+    let solve: number[] = Array<number>(s.length).fill(-1);
 
-    console.log(solution("banana"))
-    console.log(solution("poobar"))
+    [...s].forEach((char, index) => {
+        for (let j = index - 1; j >= 0; j--) {
+            if (char === s[j]) {
+                solve[index] = index - j
+                break
+            }
+        }
+    })
+
+    return solve
 }
+
+console.log(solution("banana"))
+console.log(solution("poobar"))
