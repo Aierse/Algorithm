972979c4d6ebc0e2ef5a0501846743b9dc77744b
Aierse <dbsgur2008@gmail.com>
2022-12-14 21:12:53 +0900

    refoctoring - namespace 적용

--- programmers/src/p142086_가장_가까운_같은_글자.ts
+++ programmers/src/p142086_가장_가까운_같은_글자.ts
@@ -1,17 +1,19 @@
-function solution(s: string): Array<number> {
-    let solve: number[] = Array<number>(s.length).fill(-1);
-
-    [...s].forEach((char, index) => {
-        for (let j = index - 1; j >= 0; j--) {
-            if (char === s[j]) {
-                solve[index] = index - j
-                break
+namespace p142086 {
+    function solution(s: string): Array<number> {
+        let solve: number[] = Array<number>(s.length).fill(-1);
+    
+        [...s].forEach((char, index) => {
+            for (let j = index - 1; j >= 0; j--) {
+                if (char === s[j]) {
+                    solve[index] = index - j
+                    break
+                }
             }
-        }
-    })
+        })
+    
+        return solve
+    }
 
-    return solve
+    console.log(solution("banana"))
+    console.log(solution("poobar"))
 }
-
-console.log(solution("banana"))
-console.log(solution("poobar"))
