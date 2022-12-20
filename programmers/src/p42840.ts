06a894f041b2562747a0dc8a5815fbc4ec5d0581
Aierse <dbsgur2008@gmail.com>
2022-12-20 14:37:23 +0900

    solve - 모의고사

--- programmers/src/p42840.ts
+++ programmers/src/p42840.ts
@@ -1,19 +1,13 @@
-function counting(answers: number[], human: number[]) {
-    return answers.reduce((acc, cur, idx) => {
-        if (cur === human[idx % human.length]) {
-            acc++
-        }
-
-        return acc
-    }, 0)
-}
-
 function solution(answers: number[]) {
     const h1 = [1, 2, 3, 4, 5]
     const h2 = [2, 1, 2, 3, 2, 4, 2, 5]
     const h3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    const result = [counting(answers, h1), counting(answers, h2), counting(answers, h3)]
+    const result = [
+        answers.filter((a, i) => a === h1[i % h1.length]).length, 
+        answers.filter((a, i) => a === h2[i % h2.length]).length, 
+        answers.filter((a, i) => a === h3[i % h3.length]).length,
+    ]
 
     return result.map((v, i) => {
         if (v === Math.max(...result)) return ++i
