8ab24550c617c2e8e754848fe8d488968d5e06f2
Aierse <dbsgur2008@gmail.com>
2022-12-26 14:30:16 +0900

    rename - 평행

--- programmers/src/120875.ts
+++ programmers/src/120875.ts
@@ -20,6 +20,3 @@ export function solution(dots: number[][]): number {
 
     return 0
 }
-
-console.log(solution([[1, 4], [9, 2], [3, 8], [11, 6]]))
-console.log(solution([[3, 5], [4, 1], [2, 4], [5, 10]]))
