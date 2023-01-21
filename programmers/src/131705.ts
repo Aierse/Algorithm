cc220ef219edd708cc769e83fc7823cd38e9a494
Aierse <dbsgur2008@gmail.com>
2023-01-21 10:13:53 +0900

    solve - 삼총사

--- programmers/src/131705.ts
+++ programmers/src/131705.ts
@@ -3,7 +3,7 @@ export function solution(number: number[]) {
     for(let i=0; i<number.length; i++) {
         for(let j=i+1; j<number.length; j++) {
             for(let k=j+1; k<number.length; k++) {
-                    count += number[i] + number[j] + number[k] === 0
+                    count += number[i] + number[j] + number[k] === 0 ? 1 : 0
             }
         }
     }
