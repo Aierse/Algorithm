f5abd63e37f220bfa5da635523219ca69f76b780
Aierse <68111814+Aierse@users.noreply.github.com>
2023-01-22 18:22:28 +0900

    solve - 최빈값 구하기

---
+++ programmers/src/120812.ts
@@ -0,0 +1,28 @@
+export function solution(array: number[]) {
+    let answer: { [key: number: number }= {};
+    array.forEach(v => {
+        if (answer[v] === undefined)
+            answer[v] = 1
+        else
+            answer[v]++;
+    })
+    
+    let max = 0;
+    let key = 0;
+    
+    for (let value in answer) {
+        if (max < answer[value]) {
+            max = answer[value]
+            key = value
+        }
+    }
+    
+    for (let value in answer) {
+        if (key !== value && max === answer[value])
+            key = -1
+    }
+    
+    
+    
+    return key
+}
