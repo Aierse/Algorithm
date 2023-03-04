cfa2df9cabfaef1a1aef7f96fe0aa98a33322fc6
Aierse <dbsgur2008@gmail.com>
2023-03-04 22:10:40 +0900

    solve - 점 찍기

---
+++ programmers/src/Level2/140107.ts
@@ -0,0 +1,9 @@
+export function solution(k: number, d: number) {
+  let count = 0;
+
+  for (let x = 0; x <= d; x += k) {
+    count += Math.floor(Math.floor(Math.sqrt(d ** 2 - x ** 2)) / k) + 1;
+  }
+
+  return count;
+}
