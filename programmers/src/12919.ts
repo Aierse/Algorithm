f6608ceaa05abc2962d03a186497f96bafd34880
Aierse <dbsgur2008@gmail.com>
2023-04-12 22:17:49 +0900

    solve - 서울에서 김서방 찾기

--- programmers/src/12919.ts
+++
@@ -1,3 +0,0 @@
-export function solution(seoul: string[]) {
-    return `김서방은 ${seoul.indexOf('Kim')}에 있다`
-}
---
+++ programmers/src/Level1/12919.ts
@@ -0,0 +1,3 @@
+export function solution(seoul: string[]) {
+  return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+}
