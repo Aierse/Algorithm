2ba4122a9098c7e8fb998dd5de40ff313366a463
Aierse <68111814+Aierse@users.noreply.github.com>
2023-04-30 22:24:08 +0900

    solve: 배열 비교하기

---
+++ programmers/src/Level0/181856.ts
@@ -0,0 +1,9 @@
+export function solution(arr1: number[], arr2 :number[]) {
+    if (arr1.length !== arr2.length)
+        return arr1.length > arr2.length ? 1 : -1;
+    
+    const arr1Sum = arr1.reduce((a, c) => a + c);
+    const arr2Sum = arr2.reduce((a, c) => a + c);
+    
+    return arr1Sum < arr2Sum ? -1 : arr1Sum === arr2Sum ? 0 : 1;
+}
