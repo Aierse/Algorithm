6ff225d6fb3c4320048728c4ed06556f704cfec6
Aierse <dbsgur2008@gmail.com>
2023-05-06 16:12:41 +0900

    solve - 나누어 떨어지는 숫자 배열 :+1:

--- programmers/src/12910.ts
+++
@@ -1,4 +0,0 @@
-export function solution(arr: number[], divisor: number) {
-    const answer = arr.filter(v => v % divisor === 0).sort((a, b) => a - b)
-    return answer.length === 0 ? [-1] : answer
-}
