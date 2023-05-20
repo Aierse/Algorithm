e37f498714b371f527b518706bd043487fc4ff41
Aierse <dbsgur2008@gmail.com>
2023-05-20 20:33:20 +0900

    solve - 이상한 문자 만들기

--- programmers/src/12930.ts
+++
@@ -1,7 +0,0 @@
-export function solution(s: string) {
-    return s.split(' ').map(v => 
-        v.split('').map((v, i) => {
-            return i % 2 === 0 ? v.toUpperCase() : v.toLowerCase()
-        }).join('')
-    ).join(' ')
-}
