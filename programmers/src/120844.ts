e86c8957e6557fbbca37da17d291bf0149282a85
Aierse <68111814+Aierse@users.noreply.github.com>
2023-07-07 00:33:11 +0000

    solve - 배열 회전시키기

--- programmers/src/120844.ts
+++
@@ -1,6 +0,0 @@
-export function solution(numbers: number[], direction: string) {
-    if (direction === 'right') numbers.unshift(numbers.pop()!) 
-    else numbers.push(numbers.shift()!)
-
-    return numbers
-}
---
+++ programmers/src/Level0/120844.ts
@@ -0,0 +1,6 @@
+export function solution(numbers: number[], direction: string) {
+  if (direction === "right") numbers.unshift(numbers.pop()!);
+  else numbers.push(numbers.shift()!);
+
+  return numbers;
+}
