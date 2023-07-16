d9645ab12e1c212d0f23e78a93da6cfad447d498
Aierse <dbsgur2008@gmail.com>
2023-07-16 21:33:30 +0900

    solve - 가위바위보 :+1:

--- programmers/src/120839.ts
+++
@@ -1,9 +0,0 @@
-export function solution(rsp: string) {
-    const list: { [key: string]: string } = {
-        '0': '5',
-        '2': '0',
-        '5': '2'
-    }
-
-    return [...rsp].map(v => list[v]).join('')
-}
---
+++ programmers/src/Level0/120839.ts
@@ -0,0 +1,9 @@
+export function solution(rsp: string) {
+  const list: { [key: string]: string } = {
+    "0": "5",
+    "2": "0",
+    "5": "2",
+  };
+
+  return [...rsp].map((v) => list[v]).join("");
+}
