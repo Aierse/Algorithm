f9e5091c029b29e183827abc9a1602cbbbe433ff
Aierse <dbsgur2008@gmail.com>
2023-07-23 22:23:30 +0900

    solve - 로그인 성공?

--- programmers/src/120883.ts
+++
@@ -1,8 +0,0 @@
-export function solution(id_pw: string[], db: string[]) {
-    for (const i of db) {
-        if (i[0] === id_pw[0] && i[1] === id_pw[1]) return 'login'
-        else if (i[0] === id_pw[0] && i[1] !== id_pw[1]) return 'wrong pw'
-    }
-
-    return 'fail'
-}
