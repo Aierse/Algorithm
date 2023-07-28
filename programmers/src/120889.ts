3356688569339b08c2f8ede282bebc6fa7176fae
Aierse <68111814+Aierse@users.noreply.github.com>
2023-07-28 00:28:55 +0000

    solve - 삼각형의 완성조건 (1)

--- programmers/src/120889.ts
+++
@@ -1,4 +0,0 @@
-export function solution(sides: number[]) {
-    const max = Math.max(...sides)
-    return sides.reduce((acc, cur) => acc + cur) - max <= max ? 2 : 1
-}
---
+++ programmers/src/Level0/120889.ts
@@ -0,0 +1,4 @@
+export function solution(sides: number[]) {
+  const max = Math.max(...sides);
+  return sides.reduce((acc, cur) => acc + cur) - max <= max ? 2 : 1;
+}
