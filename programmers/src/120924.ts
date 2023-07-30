f9f6195b685ffe4109351f1a6bd529bcb2d7c011
Aierse <dbsgur2008@gmail.com>
2023-07-30 13:44:45 +0900

    solve - 다음에 올 숫자

--- programmers/src/120924.ts
+++
@@ -1,6 +0,0 @@
-export function solution(common: number[]) {
-    let step = common[1] - common[0]
-    let last = common.at(-1)!
-
-    return step === common[2] - common[1] ? last + step : last * (common[2] / common[1]) / step 
-}
