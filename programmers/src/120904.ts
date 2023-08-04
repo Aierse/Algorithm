77b6e3cad762368ed44ccbef87bf6b9e38cddffe
Aierse <dbsgur2008@gmail.com>
2023-08-04 13:28:46 +0900

    solve - 숫자 찾기

--- programmers/src/120904.ts
+++
@@ -1,4 +0,0 @@
-export function solution(num: number, k: number) {
-    const i = num.toString().indexOf(k.toString())
-    return i === -1 ? -1 : i + 1
-}
---
+++ programmers/src/Level0/120904.ts
@@ -0,0 +1,4 @@
+export function solution(num: number, k: number) {
+  const i = num.toString().indexOf(k.toString());
+  return i === -1 ? -1 : i + 1;
+}
