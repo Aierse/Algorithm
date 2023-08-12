21d3a567a0d6c8f4bce731e971a4da0a932dfcb0
Aierse <dbsgur2008@gmail.com>
2023-08-12 17:16:45 +0900

    solve - OX퀴즈

--- programmers/src/120907.ts
+++
@@ -1,7 +0,0 @@
-export function solution(quiz: string[]) {
-    return quiz.map(v => {
-        let [express, result] = v.split('=')
-
-        return eval(express) === +result ? 'O' : 'X'
-    })
-}
