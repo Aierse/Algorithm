a50eba7a70ce6bc9d1f230abd384daa03a045b57
Aierse <68111814+Aierse@users.noreply.github.com>
2023-08-17 02:21:41 +0000

    solve - 캐릭터의 좌표 :+1:

---
+++ programmers/src/Level0/120861.ts
@@ -0,0 +1,26 @@
+export function solution(keyinput: string[], board: number[]) {
+  let x = 0;
+  let y = 0;
+
+  const maxX = (board[0] - 1) / 2;
+  const maxY = (board[1] - 1) / 2;
+
+  const ketMethod: { [key: string]: () => void } = {
+    right: () => {
+      if (x !== maxX) x++;
+    },
+    left: () => {
+      if (x !== -maxX) x--;
+    },
+    up: () => {
+      if (y !== maxY) y++;
+    },
+    down: () => {
+      if (y !== -maxY) y--;
+    },
+  };
+
+  keyinput.forEach((v) => ketMethod[v]());
+
+  return [x, y];
+}
