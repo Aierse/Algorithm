057cdfafb0e62af870f86ea08770d51df231a4a8
Aierse <68111814+Aierse@users.noreply.github.com>
2023-08-31 00:11:31 +0000

    solve - 숨어있는 숫자의 덧셈 (1)

--- programmers/src/120851.ts
+++
@@ -1,3 +0,0 @@
-export function solution(my_string: string) {
-    return my_string.split('').filter(v => /[\d+]/i.test(v)).reduce((acc, cur) => acc + +cur, 0)
-}
---
+++ programmers/src/Level0/120851.ts
@@ -0,0 +1,6 @@
+export function solution(my_string: string) {
+  return my_string
+    .split("")
+    .filter((v) => /[\d+]/i.test(v))
+    .reduce((acc, cur) => acc + +cur, 0);
+}
