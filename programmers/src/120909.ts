f126d28e9eb35e8c6e3978ddf7813145684ca950
Aierse <68111814+Aierse@users.noreply.github.com>
2023-09-01 01:13:23 +0000

    solve - 제곱수 판별하기

--- programmers/src/120909.ts
+++
@@ -1,3 +0,0 @@
-export function solution(n: number) {
-    return Math.floor(Math.sqrt(n)) === Math.sqrt(n) ? 1 : 2
-}
---
+++ programmers/src/Level0/120909.ts
@@ -0,0 +1,3 @@
+export function solution(n: number) {
+  return Math.floor(Math.sqrt(n)) === Math.sqrt(n) ? 1 : 2;
+}
